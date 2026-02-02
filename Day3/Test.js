1eb73f59dbe2de9acfa280d9f5eb9412af28cafe
Mayuri Lakhmapure <mayurilakhmapure12@gmail.com>
2026-02-02 21:28:11 +0530

    Day 3

---
+++ Day3/Test.js
@@ -0,0 +1,9 @@
+// Js is Dynamically typed Language
+
+let age = 30; // datatype is number
+console.log(typeof(age));
+
+age = "thirty"  // datatype of age is string
+console.log(typeof(age));
+
+console.log(age);
---
+++ Day3/Test.ts
@@ -0,0 +1,6 @@
+//TS is a statiscally typed language
+let data = 10; // number
+
+data = " ten" //compile time error
+
+console.log(data);
