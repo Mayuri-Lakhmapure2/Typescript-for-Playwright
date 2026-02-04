aec2c99eededeb5ed54f826e2879e616deddc58e
Mayuri Lakhmapure <mayurilakhmapure12@gmail.com>
2026-02-04 21:17:56 +0530

    Day 5

---
+++ Day5/ConditionalStatements.ts
@@ -0,0 +1,75 @@
+// If else
+
+    //example 1
+
+    /* let age:number=20;
+
+    if(age>=18){
+        console.log("It's an Adult.");
+    }
+    else{
+        console.log("It's not an Adult");
+    } */
+
+    
+
+        //example 2      Even odd number
+
+        /* let num:number=17;
+
+        if(num%2==0){
+            console.log(`${num} is a Even number`);
+        }
+        else{
+            console.log(`${num} is a Odd number`);
+        } */
+
+
+        //Nested if else
+
+           /*  let browser : string ="chrome";
+
+            if(browser ==="chrome"){
+                console.log("Browser is Chrome");
+            }
+            else if(browser ==="firefox"){
+                console.log("Browser is Firefox");
+            }
+            else if(browser === "Safari"){
+                console.log("Browser is Safari");
+            }
+            else{
+                console.log("Other Browser");
+            } */
+
+        //Switch Statement
+
+        let day:number=3;
+
+        switch (day){
+            case 1: 
+                console.log("Day is monday");
+                    break;
+            case 2:
+                console.log("Day is Tuesday");
+                    break;
+            case 3:
+                console.log("Day is wednesday");
+                     break;
+            case 4: 
+                console.log("Day is thursday");
+                    break;
+            case 5:
+                console.log("Day is Friday");
+                    break;
+            case 6:
+                console.log("Day is saturday");
+                    break;
+            case 7:
+                console.log("Day is Sunday");
+                    break;
+            default:
+                console.log("Invalid Day");
+
+
+        }
